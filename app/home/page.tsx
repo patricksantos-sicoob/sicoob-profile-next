86ee3caab507d4dd0314220c24a3da4ec58a4da5
Patrick Girardi dos Santos <patrick.santos@segurosicoob.com.br>
2024-10-15 16:55:55 -0300

    feat: created homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,10 @@
+import UserCard from "../components/UserCard";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen flex-col justify-center items-center bg-[#15b7a5]">
       <h1>Home</h1>
+      <UserCard />
     </div>
   );
 }
