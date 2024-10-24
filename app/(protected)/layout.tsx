9b0324dcd8ae01d7c7e3661f8d6ca5d5064cc93b
Patrick Girardi dos Santos <patrick.santos@segurosicoob.com.br>
2024-10-24 13:33:42 -0300

    refactor: just changed some colors

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -21,18 +21,18 @@ export default function HomeLayout({
   return (
     <ProtectedRoute>
       <div className="bg-[#00353e] text-white h-20 flex justify-around items-center">
-        <p className="font-semibold">Perfil Sicoob</p>
+        <p className="font-semibold text-[#7eb61d]">Perfil Sicoob</p>
         <div className="flex flex-row gap-3 items-center justify-center text-center">
           <Link className="text-white" href="/home">
             Ver perfis
           </Link>
           <Link
-            className="text-[#7eb61d]  p-2 rounded-md font-semibold"
+            className="text-white p-2 rounded-md"
             href="/user"
           >
             Minha conta
           </Link>
-          <button onClick={logout}><LogOut width={14} className="text-red-500"/></button>
+          <button className="pt-[2px]" onClick={logout}><LogOut width={14} className="text-red-500"/></button>
         </div>
       </div>
       {children}
